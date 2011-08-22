ac581f875feca1f1d7cd7977160a9652dac744bb
clseitz <>
2011-08-22 19:15:04 +0000

    makes asymmetry plots

--- TopBSM/RUTopXAna/scripts/make_asym.C
+++ TopBSM/RUTopXAna/scripts/make_asym.C
@@ -10,7 +10,7 @@
 #include "THStack.h"
 #include <sstream>
 #include "TH1F.h"
-
+#include "math.h"
 
 void CountMjjj(TH1F *temp_diagcut,float diag, float i){
 	 
@@ -30,20 +30,19 @@ void make_asym()
 
   //file 0-3 are the files to fit
   
-  filelist.push_back(TFile::Open("../TTBar_plots.root"));
+  filelist.push_back(TFile::Open("../TTBar_plots_St600.root"));
+  namelist.push_back("TTbarJEts_e");
+  //nEvtTot.push_back(216095.0);
+  nEvtTot.push_back(3.6838e+06);
+  McXsec.push_back(157.5);
+  DataLumi.push_back(lumi);
+
+  filelist.push_back(TFile::Open("../TTBar_plots_St600.root"));
   namelist.push_back("TTbarJEts_e");
   //nEvtTot.push_back(216095.0);
   nEvtTot.push_back(3.6838e+06);
   McXsec.push_back(0);
   DataLumi.push_back(lumi);
-
-  filelist.push_back(TFile::Open("../WJets_plots.root"));
-  namelist.push_back("TTbarJEts_e");
-  //nEvtTot.push_back(216095.0);
-  nEvtTot.push_back(3.6838e+06);
-  McXsec.push_back(0);
-  DataLumi.push_back(lumi);
-
   /* filelist.push_back(TFile::Open("/uscms_data/d3/clseitz/WJets/WJets_LeptonANA_e_vec_delphi_120.root"));
   nEvtTot.push_back(4.5972e+07);
   namelist.push_back("WJets_e");
@@ -51,15 +50,15 @@ void make_asym()
   DataLumi.push_back(lumi);
   */
 
-  filelist.push_back(TFile::Open("../Wp600_plots.root"));
+  filelist.push_back(TFile::Open("../Wp600_plots_St600.root"));
   namelist.push_back("Wp600_e");
   nEvtTot.push_back(99987.0);
-  McXsec.push_back(8.0);
+  McXsec.push_back(0.0);
   DataLumi.push_back(lumi);
-  filelist.push_back(TFile::Open("../Wp600_plots.root"));
+  filelist.push_back(TFile::Open("../Wp600_plots_St600.root"));
   namelist.push_back("Wp1000_e");
   nEvtTot.push_back(99995.0);
-  McXsec.push_back(0.72);
+  McXsec.push_back(0.0);
   DataLumi.push_back(lumi);
   
   //filelist.push_back(TFile::Open("/cms/data24/clseitz/MC_4_2_4/Sbottom/Sbottom750_neutralino150_LeptonAna_e25_met30_b12.root"));
@@ -91,12 +90,16 @@ void make_asym()
  VarList.push_back("LepBJet_Diag50_Upper160_4Jet_");
  VarList.push_back("LepB_Diag50_Upper160_3Jet1B_");
  VarList.push_back("LeadingJetPt_");
+ VarList.push_back("St_");
+
+   VarList.push_back("TransMassLepMET1JetB_");
+
  //VarList.push_back("TransMassLepMET4Jet_vs_LeadingJetPt_");
 
   
 
 
-  TFile fnew1("asym_plots.root", "recreate");
+  TFile fnew1("ttbar_asym_plots.root", "recreate");
   
   for(int m=0; m<VarList.size(); m++){
     vector <string > plotlist;
@@ -109,7 +112,7 @@ void make_asym()
   plotlist.push_back("4Jet_1b_M/"+VarList[m]+"4Jet_1b_M");
   plotlist.push_back("4Jet_2b_M/"+VarList[m]+"4Jet_2b_M");
 
-  for(int i=0; i<(plotlist.size()); i++){//loop for all the plots we wan to
+  for(int i=0; i<(plotlist.size()/2); i++){//loop for all the plots we wan to
    
     THStack Wp600Stack;
     THStack Wp1000Stack;
@@ -125,12 +128,12 @@ void make_asym()
       TH1F* temp;
       TH1F* tempa;
       TH1F* tempb;
-      
+      cout<<plotlist[i]<<endl;
       tempa = (TH1F*) filelist[file]->Get(plotlist[i].c_str());
-      temp =  (TH1F*) tempa->Rebin(5, "temp");
+      temp =  (TH1F*) tempa->Rebin(4, "temp");
       TH1F* temp2;
       tempb = (TH1F*) filelist[file]->Get(plotlist[i+plotlist.size()/2].c_str());
-      temp2 = (TH1F*) tempb->Rebin(5,"temp2");
+      temp2 = (TH1F*) tempb->Rebin(4,"temp2");
       //temp->Add(temp2);
       float scale = (1.0/nEvtTot[file])*DataLumi[file]*McXsec[file];
       temp->Scale(scale);
@@ -138,9 +141,12 @@ void make_asym()
       //TH1F* Asym;
 
       //temp->Draw("SAME");
-      temp->SetLineColor(file+1);
+      temp->SetLineColor(1);
       //temp->SetFillColor(file+1);
       temp->SetLineWidth(2);
+      temp2->SetLineColor(2);
+      //temp->SetFillColor(file+1);
+      temp2->SetLineWidth(2);
       //temp->GetXaxis()->SetRangeUser(0,300);
 
       if(file==4){
@@ -163,21 +169,7 @@ void make_asym()
     Wp600Stack.Add(histos[2]);
     std::stringstream cn1;
      cn1<<plotlist[i]<<"_Wprime600";
-     TCanvas* c1= new TCanvas((cn1.str()).c_str(),(cn1.str()).c_str(),800,600);
-    TLegend *leg = new TLegend(0.7060302,0.7692308,0.9761307,0.972028,NULL,"brNDC");
-    leg->AddEntry(histos[0], namelist[0].c_str(),"lep");
-    leg->AddEntry(histos[1], namelist[1].c_str(),"lep");
-    leg->AddEntry(histos[2], namelist[2].c_str(),"lep");
-
-     Wp600Stack.Draw("hist");
-     //Wp600Stack.GetXaxis()->SetRangeUser(0,300);
-     Wp600Stack.SetTitle(plotlist[i].c_str());
-     leg->Draw();
-     fnew1.cd();
-     //c1->Write();
-     std::stringstream gif;
-     gif<<plotlist[i]<<"_Wp600_e_delphi_120.gif";
-     
+  
      //c1->SaveAs((gif.str()).c_str());
 
     
@@ -191,7 +183,16 @@ void make_asym()
      Wp600StackM.Add(histosM[0]);
      Wp600StackM.Add(histosM[1]); 
      Wp600StackM.Add(histosM[2]);
-    
+     TCanvas* c2= new TCanvas((cn1.str()).c_str(),(cn1.str()).c_str(),800,600);
+     Wp600StackP.Draw("hist");
+     Wp600StackM.Draw("histsame");
+     TLegend *leg = new TLegend(0.7060302,0.7692308,0.9761307,0.972028,NULL,"brNDC");
+     leg->AddEntry(histosP[1], "postive","lep");
+     leg->AddEntry(histosM[1], "negative","lep");
+    // leg->AddEntry(histos[2], namelist[2].c_str(),"lep");
+    leg->Draw();
+     fnew1.cd();
+     c2->Write();
      TH1F* Asym;
      Asym = (TH1F*) histosP[2]->Clone();
      
@@ -199,13 +200,17 @@ void make_asym()
        //cout<<"loop"<<endl;
        Float_t binP = ((TH1*)(Wp600StackP.GetStack()->Last()))->GetBinContent(k);
        Float_t binM = ((TH1*)(Wp600StackM.GetStack()->Last()))->GetBinContent(k);
+       Float_t binE=0;
        float ratio=0;
        if(binP!=0 && binM!=0){
        ratio=(binP-binM)/(binP+binM);
+       //binE=2*sqrt((binM*binP)/pow(binM + binP,3));
+       binE=0.02;
        cout<<ratio<<" "<<binP<<" "<<binM<<endl;
        }
        else ratio=0;
        Asym->SetBinContent(k,ratio);
+       Asym->SetBinError(k,binE);
 			     
      }
      fnew1.cd();
