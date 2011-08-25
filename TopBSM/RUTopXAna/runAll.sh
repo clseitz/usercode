173f80148deea688117322f1b763737ea316e264
clseitz <>
2011-08-25 18:40:23 +0000

    making plots for all the MC

---
+++ TopBSM/RUTopXAna/runAll.sh
@@ -0,0 +1,5 @@
+./RunTopXana TTBar_plots_additionalCuts.root /cms/data24/clseitz/Ntuples/TTbarJets_*_LeptonANA_e_vec_tree_Dean.root
+./RunTopXana Wp600_plots_additionalCuts.root /cms/data24/clseitz/Ntuples/Wp600_CMS_*_LeptonANA_e_vec_tree_Dean.root
+./RunTopXana Wp1000_plots_additionalCuts.root /cms/data24/clseitz/Ntuples/Wp1000_CMS_*_LeptonANA_e_vec_tree_Dean.root
+./RunTopXana Stop350_plots_additionalCuts.root /cms/data24/clseitz/Ntuples/stop_350_pat_LeptonANA_e_vec_tree.root
+./RunTopXana Sbottom750_plots_additionalCuts.root /cms/data24/clseitz/Ntuples/pp_750_pat_LeptonANA_e_vec_tree.root
