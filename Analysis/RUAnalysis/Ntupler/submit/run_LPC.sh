863e747ca54c1d35404c3cdf895e5977c6e8edeb
clseitz <>
2012-07-06 09:05:44 +0000

    some fixes

--- Analysis/RUAnalysis/Ntupler/submit/run_LPC.sh
+++ Analysis/RUAnalysis/Ntupler/submit/run_LPC.sh
@@ -5,18 +5,19 @@ process=$1
 
 # CMSSW setup etc
 source /uscmst1/prod/sw/cms/shrc prod
+export SCRAM_ARCH="slc5_amd64_gcc462"
 
-export MYSCRIPT=/uscms/home/clseitz/MyCMS/RU/CMSSW_4_2_4/src/RUAnalysis/Ntupler/submit/ntupler_MCLPC_cfg.py
-OUTDIR=/uscms_data/d3/clseitz/T2tt
-DIR=/uscms/home/clseitz/MyCMS/RU/CMSSW_4_2_4/src/
+export MYSCRIPT=/uscms/home/clseitz/MyCMS/RU/CMSSW_5_2_2/src/RUAnalysis/Ntupler/submit/ntupler_data2012_LPC_cfg.py
+OUTDIR=/uscms_data/d3/clseitz/ThreeJet/RUNtuple/SingleMuDataset/BMu40_SHT750/
+DIR=/uscms/home/clseitz/MyCMS/RU/CMSSW_5_2_2/src/
 cd $DIR
 eval `scramv1 runtime -sh`
 #---------------------------------------------------------------
 
 echo "Running script " $MYSCRIPT 
 
-cd $DIR
-cp WpMass_process_GEN_FASTSIM_HLT_PU.py $OUTDIR.
+cp $MYSCRIPT $OUTDIR
+
 cmsRun $MYSCRIPT $process  >& $OUTDIR/Log/MCProd_${process}.log
 
 
