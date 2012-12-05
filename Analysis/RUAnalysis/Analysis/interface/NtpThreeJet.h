bb9394ef8a6bb84ec68324e73c9057e72749e7ce
cvuosalo <>
2012-12-05 17:17:58 +0000

    Changed to use Ntupler/interface/NtpReader.h

--- Analysis/RUAnalysis/Analysis/interface/NtpThreeJet.h
+++ Analysis/RUAnalysis/Analysis/interface/NtpThreeJet.h
@@ -2,7 +2,7 @@
 #define GUARD_NtpRUTopXAna_h
 
 //#include "TopBSM/RUTopXAna/interface/NtpReader.h"
-#include "RUAnalysis/Analysis/interface/NtpReader.h"
+#include "RUAnalysis/Ntupler/interface/NtpReader.h"
 #include <iostream>
 #include <vector>
 #include <fstream>
