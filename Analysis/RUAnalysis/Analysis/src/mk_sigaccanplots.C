67fb4bd61ce3d435408715289d4d0c2b540b38e9
cvuosalo <>
2013-05-08 15:34:51 +0000

    Revised to make plots for PAS

--- Analysis/RUAnalysis/Analysis/src/mk_sigaccanplots.C
+++ Analysis/RUAnalysis/Analysis/src/mk_sigaccanplots.C
@@ -19,6 +19,7 @@
 #include "TGraphErrors.h"
 #include "TGraphAsymmErrors.h"
 #include "TStyle.h"
+#include "tdrstyle.C"
 
 #include <sstream>
 
@@ -32,24 +33,23 @@ inline std::string to_string (const T& t)
 
 void mk_sigaccanplots(string flavor = "112")
 {
+	setTDRStyle();
 	// gStyle->SetOptFit(1100); // chi2 and prob, not parameters
-	gStyle->SetOptFit(1); // chi2 and prob, not parameters
+	gStyle->SetOptFit(0); // chi2 and prob, not parameters
 	// gStyle->SetOptStat("irme"); // integral, RMS, mean, # of entries
 	gStyle->SetStatFontSize(0.005);
 	gStyle->SetStatY(0.4);
-	TLatex *   tex = new TLatex(0.678392,0.9283217,"CMS Preliminary 9.95 fb^{-1} at #sqrt{s}  =  8 TeV");
+	TLatex *   tex = new TLatex(0.2,0.78,"CMS Simulation");
 	tex->SetNDC();
-	tex->SetTextAlign(22);
+	tex->SetTextAlign(12); // Left-adjusted
 	tex->SetTextFont(42);
-	// tex->SetTextSize(0.03846154);
+	float textsiz = 0.045;
+	tex->SetTextSize(textsiz);
 	tex->SetLineWidth(2);
-  cout<<"blabl"<<endl; 	
   // for(int k=0; k<3; k++){	
   // for(int k=0; k<2; k++){	
   for(int k=2; k<3; k++){	
   //Which plots to make
-  int WhichPlot=0; //0 = normal all backgrounds+error bars and data, 1= include W' on top of the background for W' mass
-  int bkgC=5;//38;
   //define input variables
   vector <string > namelist;
   vector <string > VarList;
@@ -75,13 +75,6 @@ void mk_sigaccanplots(string flavor = "112")
   vector<float> RightEdge;
   vector<vector<float  > > MassBins;
   vector <TH1F* > v_Asym;
-  char dir[99];
-  char AxisLabel[99];
-  char dirout[99];
-  float upRange=1100;
-  float lumi=2420;
-  float TriggerS = 1;
-  int nRebin=10;
 
   // string uncert="btagup";
   // if(k==1) uncert="btagdown";
@@ -96,15 +89,9 @@ void mk_sigaccanplots(string flavor = "112")
   filelist_kin.push_back(TFile::Open(("Acc_RPV" + flavor + uncert + "_Sph4.root").c_str()));
 //   filelist_kin.push_back(TFile::Open(("Acc_Gluino_NoLumi_pt60_pt110_8TeVxsec_BtagMap" + uncert + ".root").c_str()));
   
-  TFile f1(("RPV_" + flavor + "_accandwdith"+uncert+".root").c_str(), "recreate");
+  TFile f1(("RPV_" + flavor + "_accandwdithtest"+uncert+".root").c_str(), "recreate");
   f1.cd();
-
   
-  
-  
-  
-  int masses[] = {200,250,300,350,400,450,500,750, 400,450,500,750,1000,1250,1500};
-  //int masses[11] = {350,400,450,500,750,1000,1250};
   
   
   string nameIN;
@@ -113,7 +100,7 @@ void mk_sigaccanplots(string flavor = "112")
   string postfix;
   string folder;
   string ptcut;
-  folder="plots/";
+  folder="plots_test/";
   postfix=".pdf";
   /////////////Plots for each Mass separatly//////////////
   /////////////----------------------------------------/////////////
@@ -123,14 +110,6 @@ void mk_sigaccanplots(string flavor = "112")
     // if(p==1) ptcut="" + flavor;
      ptcut="60";
     if(p==1) ptcut="110";
-     ///////////////////////////////////////////////////////////////////
-      ///////////Plot nice Mjjj plots
-      //////////////////////////////////////////////////////////////////
-    int Start=0, Last = 7;
-    if(p==1) {
-    	Start=8;
-    	Last = 14;
-    }
       // string histname = string("GausWidth_vs_Mass_112") + ptcut;
       string histname = string("GausWidth_vs_Mass_" + flavor) + "_" + ptcut;
       cout<< histname << endl;
@@ -140,10 +119,12 @@ void mk_sigaccanplots(string flavor = "112")
       cout<< histname << endl;
       TGraphErrors *h_GluinoHist_MCcomb = (TGraphErrors*) filelist_kin[0]->Get(histname.c_str())->Clone();
       
-       histname = string("FullAcceptance_vs_Mass_" + flavor) + "_" + ptcut;
+      // histname = string("FullAcceptance_vs_Mass_" + flavor) + "_" + ptcut;
+      histname = string("GausMean_vs_Mass_" + flavor) + "_" + ptcut;
       // histname = string("GausAcceptance_vs_Mass_112") + ptcut;
       cout<< histname << endl;
-      TGraphErrors *h_FullAccept = (TGraphErrors*) filelist_kin[0]->Get(histname.c_str())->Clone();
+      // TGraphErrors *h_FullAccept = (TGraphErrors*) filelist_kin[0]->Get(histname.c_str())->Clone();
+      TGraph *h_FullAccept = (TGraph*) filelist_kin[0]->Get(histname.c_str())->Clone();
      
       TCanvas * cGluinoFitsOpti = new TCanvas(("RPV_"+ptcut+"_"+cuts).c_str(), ("RPV_" + ptcut+"_"+cuts).c_str(), 800, 600);
       //h_GluinoHist_Fit->SetFillColor(kOrange-2);
@@ -151,12 +132,13 @@ void mk_sigaccanplots(string flavor = "112")
 		string title;
 		string systematic = "pile-up";
 		// title="Gaussian Width vs. Mass for Light-ptcut RPV";
-		string tag = "Heavy", sphericity = " Sphericity Cut";
+		string tag = "Heavy", sphericity = " Sphericity > 0.4";
 		if (flavor.compare("112") == 0)
 			tag = "Light";
 		else if (ptcut == "60")
 			sphericity = "";
-		string titlepart = tag + "-flavor RPV " + flavor + " p_{T} = " + ptcut + sphericity;
+		string titlepart = tag + "-flavor RPV Gluino";
+		string titleln2 = "p_{T} > " + ptcut + " GeV " + sphericity;
 		title = "Width for " + titlepart;
 		/*
 		if(k==0){
@@ -175,38 +157,102 @@ void mk_sigaccanplots(string flavor = "112")
 						}
 						*/
       h_GluinoHist_Fit->SetTitle(title.c_str());
+      float titpos = 0.2;
+			TLatex *   tex2 = new TLatex(titpos, 0.89, title.c_str());
+			tex2->SetNDC();
+			tex2->SetTextAlign(12); // Left-adjusted
+			tex2->SetTextFont(42);
+			tex2->SetTextSize(textsiz);
+			tex2->SetLineWidth(2);
+			TLatex *   tex3 = new TLatex(titpos, 0.84, titleln2.c_str());
+			tex3->SetNDC();
+			tex3->SetTextAlign(12);
+			tex3->SetTextFont(42);
+			tex3->SetTextSize(textsiz);
+			tex3->SetLineWidth(2);
+			TLegend *leg;
+			if (flavor.compare("113_223") == 0 && ptcut == "110")
+				leg = new TLegend(0.592392,0.15,0.9518012,0.5,NULL,"brNDC");
+			else leg = new TLegend(0.192392,0.6847589,0.48018012,0.83553,NULL,"brNDC");
+			leg->SetBorderSize(1);
+			leg->SetTextFont(62);
+			leg->SetLineColor(0);
+			leg->SetLineStyle(1);
+			leg->SetLineWidth(1);
+			leg->SetFillColor(0);
+			leg->SetFillStyle(1001);
+			string titlepart2 = "#splitline{#splitline{#Delta = 110 GeV, 6^{th}-jet p_{T} = "
+			+ ptcut + " GeV}{";
+			// leg->SetHeader("#splitline{#splitline{#Delta = 110 GeV, 6^{th} Jet p_{T} = 110 GeV}{No b-tagging requirement}}{Sphericity > 0.4}");
+			if (flavor.compare("112") == 0)
+				titlepart2 += "No b-tag requirement";
+			else titlepart2 += ">= 1 b tag in triplet";
+			titlepart2 += "}}{" + sphericity + "}";
+			leg->SetHeader(titlepart2.c_str());
+
+
+			// leg->AddEntry(h_GluinoHist_Fit, "#splitline{Acceptance as function}{of gluino mass}","l");	
+      h_GluinoHist_Fit->SetMarkerStyle(1);
+      h_GluinoHist_Fit->SetMarkerColor(kWhite);
       // h_GluinoHist_Fit->SetTitleSize(0.01);
       h_GluinoHist_Fit->GetYaxis()->SetTitle("Width [GeV]");
       h_GluinoHist_Fit->GetYaxis()->SetTitleOffset(1.3);
-      h_GluinoHist_Fit->GetXaxis()->SetTitle("RPV Mass [GeV]");
+      h_GluinoHist_Fit->GetXaxis()->SetTitle("Gluino Mass [GeV]");
       // h_GluinoHist_Fit->Draw("AL");	
-      h_GluinoHist_Fit->Draw("A*");	
+      // h_GluinoHist_Fit->Draw("A*");	
+      h_GluinoHist_Fit->Draw("APX");	
+			// leg->Draw();
+      tex->Draw();
+      tex2->Draw();
+      tex3->Draw();
       cGluinoFitsOpti->Write();
       cGluinoFitsOpti->SaveAs((folder + "RPVwidth" +flavor + ptcut+uncert+postfix).c_str());
       TCanvas * cGluinoFitsOpt2 = new TCanvas(("RPVacc_"+ptcut+"_"+cuts).c_str(), ("RPV_" + ptcut+"_"+cuts).c_str(), 800, 600);
 			title="Acceptance for " + titlepart;
+			tex2->SetText(titpos, 0.89, title.c_str());
+      h_GluinoHist_MCcomb->SetMarkerStyle(1);
+      h_GluinoHist_MCcomb->SetMarkerColor(kWhite);
       h_GluinoHist_MCcomb->SetTitle(title.c_str());
       h_GluinoHist_MCcomb->GetYaxis()->SetTitle("Acceptance");
       h_GluinoHist_MCcomb->GetYaxis()->SetTitleOffset(1.4);
-      h_GluinoHist_MCcomb->GetXaxis()->SetTitle("RPV Mass [GeV]");
-      if (flavor.compare("113_223") == 0 && ptcut == "110") {
+      h_GluinoHist_MCcomb->GetXaxis()->SetTitle("Gluino Mass [GeV]");
+      if (false && flavor.compare("113_223") == 0 && ptcut == "110") {
       	gStyle->SetStatY(0.8);
 				h_GluinoHist_MCcomb->GetYaxis()->SetRangeUser(0.0, 0.035);
 			}
 
       // h_GluinoHist_MCcomb->Draw("AL");	
-      h_GluinoHist_MCcomb->Draw("A*");
+      h_GluinoHist_MCcomb->Draw("APX");
+      // h_GluinoHist_MCcomb->Draw("A*");
+      tex->Draw();
+      tex2->Draw();
+      tex3->Draw();
+			// leg->Draw();
       
       cGluinoFitsOpt2->Write();
       cGluinoFitsOpt2->SaveAs((folder + "RPVacc" +flavor + ptcut+uncert+postfix).c_str());
       gStyle->SetStatY(0.4);
       TCanvas * cGluinoFitsOpt3 = new TCanvas(("RPVfullacc_"+ptcut+"_"+cuts).c_str(), ("RPVfull_" + ptcut+"_"+cuts).c_str(), 800, 600);
-			title="Full Acceptance for " + titlepart;
-      h_FullAccept->SetTitle(title.c_str());
-      h_FullAccept->GetYaxis()->SetTitle("Acceptance");
-      h_FullAccept->GetYaxis()->SetTitleOffset(1.4);
-      h_FullAccept->GetXaxis()->SetTitle("RPV Mass [GeV]");
-      h_FullAccept->Draw("A*");	
+      float axsize = 0.035;
+			title="Gaussian Mean for " + titlepart;
+			tex2->SetText(titpos, 0.89, title.c_str());
+			// title="Full Acceptance for " + titlepart;
+      h_FullAccept->SetMarkerStyle(1);
+      // h_FullAccept->SetMarkerColor(kWhite);
+      h_FullAccept->SetLineColor(kRed);
+      h_FullAccept->SetLineWidth(2.0);
+      // h_FullAccept->SetTitle(title.c_str());
+      // h_FullAccept->GetYaxis()->SetTitle("Acceptance");
+      h_FullAccept->GetYaxis()->SetTitle("Gaussian Mean [GeV]");
+      h_FullAccept->GetXaxis()->SetTitleOffset(1.3);
+      h_FullAccept->GetXaxis()->SetTitle("Gluino Mass [GeV]");
+      h_FullAccept->GetXaxis()->SetTitleSize(axsize);
+      h_FullAccept->GetYaxis()->SetTitleSize(axsize);
+      h_FullAccept->GetXaxis()->SetLabelSize(axsize);
+      h_FullAccept->GetYaxis()->SetLabelSize(axsize);
+      h_FullAccept->Draw("AL");	
+      // h_FullAccept->Draw("APX");	
+			// leg->Draw();
       
       // h_GluinoHist_MCcomb->SetFillColor(10);
       // h_GluinoHist_MCcomb->SetLineColor(kBlack);
@@ -215,7 +261,9 @@ void mk_sigaccanplots(string flavor = "112")
       //f_GluinoGauss->Draw("same");
       
       //f_GluinoP4->Draw("same");
-      //tex->Draw();
+      tex->Draw();
+      tex2->Draw();
+      tex3->Draw();
       f1.cd();
 
 		
